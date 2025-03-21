a9bf7d4ba82ce607e5a4545cdb9bfb06fbb163f6
Paloma Avelino <92617070+ipami@users.noreply.github.com>
2025-03-21 18:00:18 -0300

    Validação da página de informações concluída
    Pendente de lógica para passar os dados obtidos para o backend.
    Pendente de conexão com a próxima página.

--- app/src/main/java/com/example/tmt/InformationActivity.kt
+++ app/src/main/java/com/example/tmt/InformationActivity.kt
@@ -1,20 +1,43 @@
 package com.example.tmt
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class InformationActivity : AppCompatActivity() {
+
+    lateinit var patientName : EditText
+    lateinit var patientAge: EditText
+    lateinit var profissionalName : EditText
+    lateinit var btnConfirm : Button
+    lateinit var btnBackMain : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_information)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        patientName = findViewById(R.id.inputPatientName)
+        patientAge = findViewById(R.id.inputPatientAge)
+        profissionalName = findViewById(R.id.inputProfessionalName)
+        btnConfirm = findViewById(R.id.buttonConfirm)
+        btnBackMain = findViewById(R.id.buttonBackMain)
+
+        btnConfirm.setOnClickListener {
+            if (patientName.text.toString().isEmpty()) {
+                patientName.error = "Nome do paciente é obrigatório"
+            } else if (patientAge.text.toString().isEmpty()) {
+                patientAge.error = "Idade é obrigatória"
+            } else if (profissionalName.text.toString().isEmpty()) {
+                profissionalName.error = "Nome do aplicador é obrigatório"
+            }
+        }
+
+        btnBackMain.setOnClickListener {
+            finish()
         }
     }
 }
