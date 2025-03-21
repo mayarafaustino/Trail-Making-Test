e1a34e531425c0317d0a8692ffef660c3f10e663
Paloma Avelino <92617070+ipami@users.noreply.github.com>
2025-03-20 23:57:40 -0300

    Layout da Página de Informações
    Layout da página de informações está completo. Pendente lógica e validação.

--- app/src/main/java/com/example/tmt/HomeActivity.kt
+++ app/src/main/java/com/example/tmt/HomeActivity.kt
@@ -1,20 +1,30 @@
 package com.example.tmt
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var btnEnter : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnEnter = findViewById(R.id.btn_home)
+
+        btnEnter.setOnClickListener {
+
+            val intent = Intent(this, InformationActivity::class.java)
+
+            startActivity(intent)
         }
+
     }
 }
